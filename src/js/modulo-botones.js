5fc6244a59fba3356da18be9392d739b6fb77c7f
sebagorito <sebagorito@gmail.com>
2026-06-24 12:51:54 -0300

    Implementacion completa carrito de compras y filtros

--- src/js/modulo-botones.js
+++ src/js/modulo-botones.js
@@ -1,39 +1,42 @@
-export function setearBoton(){
-    let boton = document.getElementById("botonPerfil");
-    configurarBoton(boton);
-    boton.addEventListener("click", controlIngreso)
+export function setearBoton() {
+  let boton = document.getElementById("botonPerfil");
+  configurarBoton(boton);
+  boton.addEventListener("click", controlIngreso);
 }
 
-function configurarBoton(boton){
-    if (localStorage.getItem("usuarioActivo") == undefined || localStorage.getItem("usuarioActivo") == "undefined"){
-        boton.innerText = "Iniciar Sesion";
-    }else boton.innerText = "Perfil"
+function configurarBoton(boton) {
+  if (
+    localStorage.getItem("usuarioActivo") == undefined ||
+    localStorage.getItem("usuarioActivo") == "undefined"
+  ) {
+    boton.innerText = "Iniciar Sesion";
+  } else boton.innerText = "Perfil";
 }
 
-function controlIngreso(){
-    let usuarioActivo = localStorage.getItem("usuarioActivo"); 
-    switch (usuarioActivo) {
-        case null:{
-            localStorage.setItem("usuarioActivo", undefined);        
-            window.location.href = "ingreso-usuario.html"
-            break;
-        }
-        case "undefined":
-            window.location.href = "ingreso-usuario.html";
-            break;
-        default:{
-            if (JSON.parse(usuarioActivo).admin){
-                window.location.href = "admin-index.html"
-            }else window.location.href = "perfil-usuario.html"
-            break;
-        }
+function controlIngreso() {
+  let usuarioActivo = localStorage.getItem("usuarioActivo");
+  switch (usuarioActivo) {
+    case null: {
+      localStorage.setItem("usuarioActivo", undefined);
+      window.location.href = "ingreso-usuario.html";
+      break;
     }
+    case "undefined":
+      window.location.href = "ingreso-usuario.html";
+      break;
+    default: {
+      if (JSON.parse(usuarioActivo).admin) {
+        window.location.href = "admin-index.html";
+      } else window.location.href = "perfil-usuario.html";
+      break;
+    }
+  }
 }
 
-export function botonCerrarSesion(){
-    let boton = document.getElementById("botonCerrarSesion");
-    boton.addEventListener("click", function(){
-        localStorage.setItem("usuarioActivo", undefined);
-        window.location.href = "index.html"
-    })
+export function botonCerrarSesion() {
+  let boton = document.getElementById("botonCerrarSesion");
+  boton.addEventListener("click", function () {
+    localStorage.setItem("usuarioActivo", undefined);
+    window.location.href = "index.html";
+  });
 }
